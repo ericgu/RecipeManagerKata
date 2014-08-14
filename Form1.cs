5aa13f7186435a653896de13ac87f4f41f4dd9ee
ericgu <eric_gunnerson@hotmail.com>
2014-08-14 13:51:05 -0700

    Initial version

---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecipeManager
+{
+    public partial class Form1 : Form
+    {
+        private List<Recipe> m_recipes = new List<Recipe>(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadRecipes();
+        }
+
+        private void LoadRecipes()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(@"e:\portkata");
+            m_recipes = directoryInfo.GetFiles("*")
+                .Select(fileInfo => new Recipe { Name = fileInfo.Name, Size = fileInfo.Length, Text = File.ReadAllText(fileInfo.FullName) }).ToList();
+
+            PopulateList();
+        }
+
+        private void PopulateList()
+        {
+            listView1.Items.Clear();
+
+            foreach (Recipe recipe in m_recipes)
+            {
+                listView1.Items.Add(new RecipeListViewItem(recipe));
+            }
+        }
+
+        private void DeleteClick(object sender, EventArgs e)
+        {
+            foreach (RecipeListViewItem recipeListViewItem in listView1.SelectedItems)
+            {
+                m_recipes.Remove(recipeListViewItem.Recipe);
+                File.Delete(@"e:\portkata\" + recipeListViewItem.Recipe.Name);
+            }
+            PopulateList();
+
+            NewClick(null, null);
+        } 
+
+        private void NewClick(object sender, EventArgs e)
+        {
+            textBoxName.Text = "";
+            textBoxObjectData.Text = "";
+        }
+
+        private void SaveClick(object sender, EventArgs e)
+        {
+            File.WriteAllText(Path.Combine("e:\\portkata", textBoxName.Text), textBoxObjectData.Text);
+            LoadRecipes();
+        }
+
+        private void SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (RecipeListViewItem recipeListViewItem in listView1.SelectedItems)
+            {
+                textBoxName.Text = recipeListViewItem.Recipe.Name;
+                textBoxObjectData.Text = recipeListViewItem.Recipe.Text;
+                break;
+            }
+        }
+    }
+}
