b9c654aebbf417dc3a5461e063ccd6afa99e0749
ericgu <eric_gunnerson@hotmail.com>
2014-10-29 09:34:04 -0700

    Created simulator class.

---
+++ RecipeStoreSimulator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeManager
+{
+    class RecipeStoreSimulator : IRecipeStore
+    {
+        public List<Recipe> LoadRecipes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRecipe(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveRecipe(string name, string contents)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
