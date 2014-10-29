6227de2389351865bcd1b286b1ed8554b7b6bfa5
ericgu <eric_gunnerson@hotmail.com>
2014-10-29 10:07:05 -0700

    Renamed all simulators using field convention

--- RecipeStoreSimulator_Tests.cs
+++ RecipeStoreSimulator_Tests.cs
@@ -10,81 +10,79 @@ namespace RecipeManager
     [TestClass]
     public class RecipeStoreSimulator_Tests
     {
+        private RecipeStoreSimulator m_simulator = new RecipeStoreSimulator();
+
         [TestMethod()]
         public void When_I_create_a_RecipeStoreSimulator__it_is_not_null()
         {
-            RecipeStoreSimulator simulator = new RecipeStoreSimulator();
-
-            Assert.IsNotNull(simulator);
+            Assert.IsNotNull(m_simulator);
         }
 
         [TestMethod()]
         public void When_I_create_a_RecipeStoreSimulator_and_call_LoadRecipes__it_returns_an_empty_list()
         {
-            RecipeStoreSimulator simulator = new RecipeStoreSimulator();
+            RecipeStoreSimulator m_simulator = new RecipeStoreSimulator();
 
-            var recipes = simulator.LoadRecipes();
+            var recipes = m_simulator.LoadRecipes();
 
-            Assert.AreEqual(0, recipes.Count);
+            ListContainsRecipesInOrder(recipes);
         }
 
         [TestMethod()]
         public void When_I_call_SaveRecipe_and_call_LoadRecipes__it_returns_that_recipe()
         {
-            RecipeStoreSimulator simulator = new RecipeStoreSimulator();
+            RecipeStoreSimulator m_simulator = new RecipeStoreSimulator();
 
             Recipe recipe = new Recipe {Name = "Eggs", Text = "Cook eggs", Size = 9};
-            simulator.SaveRecipe(recipe);
+            m_simulator.SaveRecipe(recipe);
 
-            var recipes = simulator.LoadRecipes();
+            var recipes = m_simulator.LoadRecipes();
 
-            Assert.AreEqual(1, recipes.Count);
-            CompareRecipeToRecipeInList(recipe, recipes, 0);
-        }
-
-        private static void CompareRecipeToRecipeInList(Recipe recipe, List<Recipe> recipes, int recipeIndex)
-        {
-            Assert.AreEqual(recipe.Name, recipes[recipeIndex].Name);
-            Assert.AreEqual(recipe.Text, recipes[recipeIndex].Text);
-            Assert.AreEqual(recipe.Size, recipes[recipeIndex].Size);
+            ListContainsRecipesInOrder(recipes, recipe);
         }
 
         [TestMethod()]
         public void When_I_call_SaveRecipe_twice_and_call_LoadRecipes__it_returns_both_recipes()
         {
-            RecipeStoreSimulator simulator = new RecipeStoreSimulator();
+            RecipeStoreSimulator m_simulator = new RecipeStoreSimulator();
 
             Recipe recipe1 = new Recipe { Name = "Ham", Text = "Slice Ham", Size = 9 };
-            simulator.SaveRecipe(recipe1);
+            m_simulator.SaveRecipe(recipe1);
             Recipe recipe2 = new Recipe { Name = "Eggs", Text = "Cook eggs", Size = 9 };
-            simulator.SaveRecipe(recipe2);
+            m_simulator.SaveRecipe(recipe2);
 
-            var recipes = simulator.LoadRecipes();
+            var recipes = m_simulator.LoadRecipes();
 
-            Assert.AreEqual(2, recipes.Count);
-
-            CompareRecipeToRecipeInList(recipe1, recipes, 0);
-            CompareRecipeToRecipeInList(recipe2, recipes, 1);
+            ListContainsRecipesInOrder(recipes, recipe1, recipe2);
         }
-    
+
         [TestMethod()]
         public void When_I_call_SaveRecipe_twice_Delete_one_recipe_and_call_LoadRecipes__it_returns_one_recipe()
         {
-            RecipeStoreSimulator simulator = new RecipeStoreSimulator();
+            RecipeStoreSimulator m_simulator = new RecipeStoreSimulator();
 
             Recipe recipe1 = new Recipe { Name = "Ham", Text = "Slice Ham", Size = 9 };
-            simulator.SaveRecipe(recipe1);
+            m_simulator.SaveRecipe(recipe1);
             Recipe recipe2 = new Recipe { Name = "Eggs", Text = "Cook eggs", Size = 9 };
-            simulator.SaveRecipe(recipe2);
+            m_simulator.SaveRecipe(recipe2);
 
-            simulator.DeleteRecipe(recipe1);
+            m_simulator.DeleteRecipe(recipe1);
 
-            var recipes = simulator.LoadRecipes();
+            var recipes = m_simulator.LoadRecipes();
 
-            Assert.AreEqual(1, recipes.Count);
+            ListContainsRecipesInOrder(recipes, recipe2);
+        }
 
-            CompareRecipeToRecipeInList(recipe2, recipes, 0);
+        private static void ListContainsRecipesInOrder(List<Recipe> recipes, params Recipe[] expectedRecipes)
+        {
+            Assert.AreEqual(expectedRecipes.Length, recipes.Count);
+
+            for (int i = 0; i < expectedRecipes.Length; i++)
+            {
+                Assert.AreEqual(expectedRecipes[i].Name, recipes[i].Name);
+                Assert.AreEqual(expectedRecipes[i].Text, recipes[i].Text);
+                Assert.AreEqual(expectedRecipes[i].Size, recipes[i].Size);
+            }
         }
     }
-
 }
