cc6d3aed2775f26fd9e7a507a16b2efa2b8f96eb
ericgu <eric_gunnerson@hotmail.com>
2014-10-29 13:55:29 -0700

    Inlined creation of store and UI.

--- Form1.cs
+++ Form1.cs
@@ -14,15 +14,14 @@ namespace RecipeManager
     public partial class Form1 : Form
     {
         private RecipeManager m_recipeManager;
-        private readonly RecipeStore m_recipeStore = new RecipeStore(@"e:\portkata");
-        private RecipeUserInterface m_recipeUserInterface;
 
         public Form1()
         {
             InitializeComponent();
 
-            m_recipeUserInterface = new RecipeUserInterface(listView1, textBoxName, textBoxObjectData, buttonNew, buttonSave, buttonDelete);
-            m_recipeManager = new RecipeManager(m_recipeStore, m_recipeUserInterface);
+            m_recipeManager = new RecipeManager(
+                                    new RecipeStore(@"e:\portkata"), 
+                                    new RecipeUserInterface(listView1, textBoxName, textBoxObjectData, buttonNew, buttonSave, buttonDelete));
         }
     }
 }
